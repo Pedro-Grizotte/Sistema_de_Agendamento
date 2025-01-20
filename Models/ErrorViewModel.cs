0c3e662c3dede2137a710b44a4183516ba4555c5
Pedro Grizotte <pgrizotte@gmail.com>
2025-01-20 19:49:43 -0300

    Começo do Projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sistema_de_Agendamento.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
